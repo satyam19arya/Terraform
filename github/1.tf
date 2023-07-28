be399df288e7f134b4fcfc74d17afbed028492cb
Satyam <satyam19arya@gmail.com>
2023-07-29 00:52:21 +0530

    updated

---
+++ github/1.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+    token = ""
+}
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+  visibility = "public"
+  auto_init  = false
+}
