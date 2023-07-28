dc5326f3b7ceed1699ff3b6add7cca29d6cff820
Satyam <satyam19arya@gmail.com>
2023-07-28 22:17:27 +0530

    updated

--- 2_variables.tf
+++
@@ -1,9 +0,0 @@
-variable username {}
-
-output printname1 {
-    value = "Hello, ${var.username}"
-}
-
-output printname2 {
-    value = var.username
-}
---
+++ basics/2_variables.tf
@@ -0,0 +1,12 @@
+variable username {}
+
+output printname1 {
+    value = "Hello, ${var.username}"
+}
+
+output printname2 {
+    value = var.username
+}
+
+# Now we want to supply the variable value from the command line. so that we can run terraform plan command in non-interactive node.
+# terraform plan -var "username=satyam"
