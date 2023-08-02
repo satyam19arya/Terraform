f39f9542d7ba90726a81acea0eaf2b5c6d67c454
Satyam <satyam19arya@gmail.com>
2023-08-02 12:58:13 +0530

    updated

--- s3_website_hosting/main.tf
+++ s3_website_hosting/main.tf
@@ -6,14 +6,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.s3_bucket.id
   acl    = "private"
